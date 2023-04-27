bccd4d770b8b65f7c2693e68d42bf890c2bd764e
Extocine <87915650+Extocine@users.noreply.github.com>
2023-04-26 19:22:32 -0500

    Update config.h

--- dwm/config.h
+++ dwm/config.h
@@ -2,16 +2,17 @@
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int snap      = 12;       /* snap pixel */
+static const unsigned int gappx     = 5;        /* gaps between windows */
+static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
+static const char col_gray1[]       = "#222222"; /* The grey behind the Icons */
 static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#46c8c0";
+static const char col_gray3[]       = "#bbbbbb"; /*Icon Colors*/ /*bbbbbb*/
+static const char col_gray4[]       = "#222222"; /*Window Title Text Color*/
+static const char col_cyan[]        = "#5aecf9"; /*Purple 5a32a8*/
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
@@ -19,7 +20,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "", "", "", "", "", "", "", "", "" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -27,8 +28,16 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "discord",     NULL,       NULL,       1 << 2,             0,       -1 },
+	{ "Google-chrome",  NULL,    NULL,       1 << 1,             0,       -1 },
+	{ "Google Chrome", NULL, NULL,		 1 << 1,		0,	 -1 },
+	{ "Thunar",	NULL,	NULL,		 1 << 4,		0,	 -1 },
+	{ "Steam", NULL,	NULL,		 1 << 3,		0,	 -1 },
+	{ "Spotify", "spotify",	"Spotify",	 1 << 5,		0,	 -1 },
+	{ "KeePassXC", NULL,	NULL,		 1 << 8,		0,	 -1 },
+	{ "Minecraft Launcher", NULL, NULL, 	 1 << 3,		0, 	 -1 },
+	{ "Veracrypt", NULL, 	NULL, 		 1 << 4,		0,	 -1 },
+	{ "Pavucontrol", NULL,	NULL,		 1 << 8,		0,	 -1 },
 };
 
 /* layout(s) */
@@ -40,7 +49,7 @@ static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen win
 static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ "[T]",      tile },    /* first entry is default */
-	{ "[X]",      NULL },    /* no layout function means floating behavior */
+	{ "[F]",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
 };
 
@@ -59,13 +68,15 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
-static const char *lightpadcmd[] = { "com.github.libredeb.lightpad", NULL };
+static const char *xfce4appfindercmd[] = { "xfce4-appfinder", NULL };
 static const char *screenshotcmd[] = { "sudo", "flameshot", "gui", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,			XK_Super_L, spawn,	   {.v = xfce4appfindercmd } },
+	{ MODKEY|ShiftMask, 		XK_p, 	   spawn, 	   {.v = screenshotcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = -1 } },
@@ -87,8 +98,9 @@ static Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	{ MODKEY,			XK_Super_L, spawn,	   {.v = lightpadcmd } },
-	{ MODKEY|ShiftMask, 		XK_p, 	   spawn, 	   {.v = screenshotcmd } },
+	{ MODKEY,                       XK_minus,  setgaps,        {.i = -1 } },
+	{ MODKEY,                       XK_equal,  setgaps,        {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = 0  } },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
